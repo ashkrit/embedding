c1373eee48e14c89872eb34e53eb37c9e770b60d
ashkrit@gmail.com <ashkrit@gmail.com>
2024-03-13 15:50:30 +0800

    Add headers

--- client/src/main/java/org/genai/api/EmbeddingService.java
+++ client/src/main/java/org/genai/api/EmbeddingService.java
@@ -6,12 +6,15 @@ import org.genai.protocol.ModelInfo;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface EmbeddingService {
     @GET("list")
+    @Headers({"Content-Type: application/json"})
     Call<ModelInfo> listModels();
 
     @POST("embedding")
+    @Headers({"Content-Type: application/json"})
     Call<EmbeddingReply> embedding(@Body Embedding embedding);
 }
--- client/src/main/java/org/genai/api/GoogleAIService.java
+++ client/src/main/java/org/genai/api/GoogleAIService.java
@@ -3,13 +3,12 @@ package org.genai.api;
 import org.genai.protocol.GoogleEmbedding;
 import org.genai.protocol.GoogleEmbedding.GoogleEmbeddingReply;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.*;
 
 public interface GoogleAIService {
 
     @POST("/v1beta/models/embedding-001:embedContent")
+    @Headers({"Content-Type: application/json"})
     Call<GoogleEmbeddingReply> embedding(@Query("key") String apiKey, @Body GoogleEmbedding embedding);
 
 }
